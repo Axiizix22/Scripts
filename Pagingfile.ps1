f4d404080b4b5a33248d2651747df4e98863f652
Axel GLEIZES <axiizix@gmail.com>
2025-02-19 14:18:15 +0100

    Add files via upload

---
+++ Pagingfile.ps1
@@ -0,0 +1,13 @@
+#Get LogicDisks for machine with Temporary Disks
+$LogicalDisks = Get-WmiObject Win32_LogicalDisk | Where-Object VolumeName -EQ 'Windows'
+
+#Set Pagefile on Windows disk if exists
+If ($LogicalDisks){
+    
+    Write-Host "Setting PageFile on $($LogicalDisks.DeviceID)"
+    $pagefileset = Get-WmiObject win32_pagefilesetting | Where-Object{$_.caption -like "$($LogicalDisks.DeviceID)*"}
+    $pagefileset.InitialSize = 6144
+    $pagefileset.MaximumSize = 24576
+    $pagefileset.Put() | Out-Null
+
+}
